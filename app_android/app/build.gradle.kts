3cbf3840245db440052b9fe002d3df2e0975eb5d
et11 <adrian.walczak119@gmail.com>
2023-11-30 15:22:26 +0000

    Creating connection with Broker

--- app_android/app/build.gradle.kts
+++ app_android/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.app_android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.app_android"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app_android/app/src/main/java/com/example/app_android/MainActivity.java
+++ app_android/app/src/main/java/com/example/app_android/MainActivity.java
@@ -4,18 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BROCKER_URL = "tcp://broker.hivemq.com:1883";//"tcp://mqtt-dashboard.com:8884"
+    private static final String CLIENT_ID = "adrr114unif";
+    private MqttHandler mqttHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mqttHandler = new MqttHandler();
+        mqttHandler.connect(BROCKER_URL,CLIENT_ID);
+
     }
 
-    public void switch_light(View v) {
-        Intent intent = new Intent(this, Chat.class);
+    @Override
+    protected void onDestroy(){
+        mqttHandler.disconnect();
+        super.onDestroy();
+    }
 
-        String name_temp = name.getText().toString();
+    private void publishMessage(String topic, String message){
+        Toast.makeText(this,"Publishing message: " + message, Toast.LENGTH_SHORT).show();
+        mqttHandler.publish(topic,message);
+    }
+
+    private void subscribeToTopic(String topic){
+        Toast.makeText(this,"Subscribe to topic "+topic,Toast.LENGTH_SHORT).show();
+        mqttHandler.subscribe(topic);
+    }
+    public void switch_light(View v) {
+        publishMessage("light","red");
     }
 }
